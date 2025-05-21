97c96e4339868ed9d44bfc6e2baac4f903cdfae6
Styl Bandeira <stylbandeira@gmail.com>
2025-05-21 19:01:19 -0300

    Create produtos routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\VariacaoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('variacoes', VariacaoController::class);
+Route::resource('produtos', ProdutoController::class);
