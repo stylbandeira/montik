634478cdab176d22d1a04ec09e56bd8c1e7e4a4a
Styl Bandeira <stylbandeira@gmail.com>
2025-05-22 17:58:43 -0300

    Adds opcoesvariacoes routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\OpcoesVariacoesController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\VariacaoController;
+use App\Models\OpcoesVariacoes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +24,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::resource('variacoes', VariacaoController::class);
 Route::resource('produtos', ProdutoController::class);
+Route::resource('opcoesvariacoes', OpcoesVariacoesController::class);
