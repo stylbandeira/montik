a236f2b962c52335cf6dd53653ddb8e718138129
Styl Bandeira <stylbandeira@gmail.com>
2025-05-22 10:56:18 -0300

    Creates OpcoesVariacoes, Pedido and Produto models

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -6,15 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Produto extends Model
+class Pedido extends Model
 {
     use HasFactory;
     use SoftDeletes;
-
-    protected $table = 'produtos';
-    protected $fillable = [
-        'nome',
-        'preco',
-        'descricao'
-    ];
 }
