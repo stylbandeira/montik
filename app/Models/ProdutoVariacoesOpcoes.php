18860b98412e747ea69823da829f6fe86262f105
Styl Bandeira <stylbandeira@gmail.com>
2025-05-23 09:23:24 -0300

    Create models

---
+++ app/Models/ProdutoVariacoesOpcoes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProdutoVariacoesOpcoes extends Model
+{
+    use HasFactory;
+    protected $table = 'produto_variacoes_opcoes';
+}
