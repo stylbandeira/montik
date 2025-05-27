6a2617c9f71a927839f891508cdc7690af258905
Styl Bandeira <stylbandeira@gmail.com>
2025-05-27 08:29:06 -0300

    Add produto and opcao relationship

--- app/Models/ProdutoVariacoesOpcoes.php
+++ app/Models/ProdutoVariacoesOpcoes.php
@@ -9,4 +9,14 @@ class ProdutoVariacoesOpcoes extends Model
 {
     use HasFactory;
     protected $table = 'produto_variacoes_opcoes';
+
+    public function produto()
+    {
+        return $this->belongsTo(Produto::class, 'id_produto_variacoes', 'id');
+    }
+
+    public function opcao()
+    {
+        return $this->belongsTo(OpcoesVariacoes::class, 'id_opcoes_variacoes', 'id');
+    }
 }
