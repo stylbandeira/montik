8f46738559476dc301a8a7b1e8159096ed773df7
Styl Bandeira <stylbandeira@gmail.com>
2025-05-27 08:28:23 -0300

    Add produto and produto_variacoes relationship on Estoque

--- app/Models/Estoque.php
+++ app/Models/Estoque.php
@@ -9,4 +9,14 @@ class Estoque extends Model
 {
     use HasFactory;
     protected $table = 'estoque';
+
+    public function produto()
+    {
+        return $this->belongsTo(Produto::class, 'id_produto', 'id');
+    }
+
+    public function produto_variacoes()
+    {
+        return $this->belongsTo(ProdutoVariacoes::class, 'id_produto_variacoes', 'id');
+    }
 }
