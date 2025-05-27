51d189911dc2dfbc04bca0392403dac296ad376f
Styl Bandeira <stylbandeira@gmail.com>
2025-05-27 08:28:53 -0300

    Add opcoes and estoque relationship

--- app/Models/ProdutoVariacoes.php
+++ app/Models/ProdutoVariacoes.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class ProdutoVariacoes extends Model
 {
     use HasFactory;
+
+    public function opcoes()
+    {
+        return $this->hasMany(ProdutoVariacoesOpcoes::class, 'id_produto_variacoes');
+    }
+
+    public function estoque()
+    {
+        return $this->hasOne(Estoque::class, 'id_produto_variacoes');
+    }
 }
