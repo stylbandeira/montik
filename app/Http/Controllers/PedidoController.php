8495d4648247461437a7d5c6861eb2fc4b4b4825
Styl Bandeira <stylbandeira@gmail.com>
2025-05-29 11:29:02 -0300

    Begins create and store on PedidoController

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Estoque;
+use App\Models\PrePedido;
+use App\Models\PrePedidoItens;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use Inertia\Inertia;
 
 class PedidoController extends Controller
@@ -22,7 +27,22 @@ class PedidoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create() {}
+    public function create(Request $request)
+    {
+        //PEGAR EMAIL
+        //PEGAR CEP E ENDEREÇO
+        if (!$request->email || $request->user()->email) {
+            return response([
+                'message' => 'Email necessário'
+            ]);
+        }
+
+        if ($request->user()) {
+        } else {
+            // CRIA UM PRE_PEDIDO OU RECUPERA O PRE-PEDIDO BASEADO NO UUID
+            $pre_pedido = PrePedido::firstOrNew(['uuid' => $request->uuid]);
+        }
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -32,20 +52,42 @@ class PedidoController extends Controller
      */
     public function store(Request $request)
     {
-        if (!$request->user()) {
-            //GUARDA NUMA TABELA DE PEDIDOS TEMPORÁRIOS (PRE_PEDIDO_ITENS)
-            dd('AA');
-            //REDIRECIONA PARA O CADASTRO E GUARDA OS DADOS TEMPORÁRIOS (PRE_PEDIDO)
-            return Inertia::render();
+        //VERIFICA SE O USUÁRIO ESTÁ LOGADO OU NÃO
+        if ($request->user()) {
+        } else {
+            // CRIA UM PRE_PEDIDO OU RECUPERA O PRE-PEDIDO BASEADO NO UUID
+            $pre_pedido = PrePedido::firstOrNew(['uuid' => $request->uuid]);
         }
 
-        //CRIA UM CARRINHO ATRIBUÍDO AO USUÁRIO LOGADO
-        //OU
-        //TRANSFERE O PRE_PEDIDO_ITENS PARA O CARRINHO E SOFTDELETA O REGISTRO EM PRE_PEDIDO_ITENS
+        foreach ($request->carrinho as $produtos) {
 
-        //AO FINALIZAR O CADASTRO, ATRIBUIR O PEDIDO AO USUÁRIO CRIADO
-        //AO FAZER O LOGIN, O USUÁRIO PODERÁ ACESSAR O PEDIDO DELE
-        //
+            $estoque = Estoque::where('id_produto_variacoes', $produtos['id_produto_variacao'])->first();
+
+            //VERIFICA SE EXISTE A QUANTIDADE SOLICITADA DE ITENS DAQUELA VARIAÇÃO EM ESTOQUE
+            if (!$estoque || ($estoque->quantidade < $produtos['quantidade'])) {
+                return response([
+                    'message' => 'Produto em falta no estoque'
+                ], 400);
+            }
+
+            //CASO EXISTA AQUELA QUANTIDADE, O ESTOQUE É ATUALIZADO E ADICIONADO AO PRE_PEDIDO_ITENS
+            $estoque->quantidade -= $produtos['quantidade'];
+            // $estoque->save();
+
+            $pre_pedido_itens = new PrePedidoItens();
+            $pre_pedido_itens->quantidade = $produtos['quantidade'];
+            $pre_pedido_itens->id_produto_variacoes = $produtos['id_produto_variacao'];
+            $pre_pedido_itens->id_pre_pedido = $pre_pedido->id;
+            // $pre_pedido->itens->save();
+
+        }
+
+        //REDIRECIONA PARA A PÁGINA DE CHECKOUT
+        $this->create();
+
+        //####### PARA DEPOIS ###########
+        //DEVE SER CRIADO UM JOB PARA REMOVER OS PEDIDOS EM PRE_PEDIDO QUE TENHAM MAIS DE 3 DIAS DE CRIAÇÃO
+
     }
 
     /**
