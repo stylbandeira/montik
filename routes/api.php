5ff503daee2745a35649fa627abeac3785033895
Styl Bandeira <stylbandeira@gmail.com>
2025-05-29 07:42:38 -0300

    Adds produtos/variacoes-disponiveis route on API

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\EstoqueController;
 use App\Http\Controllers\OpcoesVariacoesController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\VariacaoController;
+use App\Models\Estoque;
 use App\Models\OpcoesVariacoes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,6 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::resource('variacoes', VariacaoController::class);
+Route::get('produtos/variacoes-disponiveis', [ProdutoController::class, 'variacoesDisponiveis']);
 Route::resource('produtos', ProdutoController::class);
 Route::resource('opcoesvariacoes', OpcoesVariacoesController::class);
 Route::resource('estoques', EstoqueController::class);
