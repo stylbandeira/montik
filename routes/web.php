3e5e91d62774057eb34219e6a4fed304792906c1
Styl Bandeira <stylbandeira@gmail.com>
2025-05-29 11:30:14 -0300

    Create pedidos store route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\EstoqueController;
+use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
@@ -54,5 +55,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/pedidos', [PedidoController::class, 'store'])->name('pedido.store');
 
 require __DIR__ . '/auth.php';
