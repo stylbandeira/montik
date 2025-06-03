edf26de573a24bb930e7e228447473ad0546ca68
Styl Bandeira <stylbandeira@gmail.com>
2025-06-03 08:03:58 -0300

    Begins Pedido storage

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Endereco;
 use App\Models\Estoque;
 use App\Models\PrePedido;
 use App\Models\PrePedidoItens;
@@ -53,39 +54,95 @@ class PedidoController extends Controller
      */
     public function store(Request $request)
     {
+        Log::alert($request->all());
+        //VALIDA OS DADOS DE ENDEREÇO
+        $validator = Validator::make($request->all(), [
+            'rua' => 'required|string',
+            'numero' => 'required|string',
+            'complemento' => 'required|string',
+            'bairro' => 'required|string',
+            'cidade' => 'required|string',
+            'estado' => 'required|string',
+            'cep' => 'required|integer',
+            'frete' => 'required|decimal:2:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response([
+                'errors' => $validator->errors()
+            ]);
+        }
+
         //VERIFICA SE O USUÁRIO ESTÁ LOGADO OU NÃO
         if ($request->user()) {
         } else {
             // CRIA UM PRE_PEDIDO OU RECUPERA O PRE-PEDIDO BASEADO NO UUID
             $pre_pedido = PrePedido::firstOrNew(['uuid' => $request->uuid]);
+            // SE FOR O MESMO UUID E O MESMO ENDEREÇO, APENAS PEGA O ENDEREÇO.
+            $endereco = Endereco::firstOrNew([
+                'uuid' => $request->uuid,
+                'rua' => $request->rua,
+                'numero' => $request->numero,
+                'complemento' => $request->complemento,
+                'bairro' => $request->bairro,
+                'cidade' => $request->cidade,
+                'estado' => $request->estado,
+                'cep' => $request->cep,
+            ]);
+
+            $pre_pedido->id_endereco = $endereco->id;
+            $pre_pedido->frete = $request->frete;
         }
 
+        //CRIA O ENDEREÇO ANYWAY
+        $endereco->rua = $request->rua;
+        $endereco->numero = $request->numero;
+        $endereco->complemento = $request->complemento;
+        $endereco->bairro = $request->bairro;
+        $endereco->cidade = $request->cidade;
+        $endereco->estado = $request->estado;
+        $endereco->cep = $request->cep;
+        // $endereco->save();
+
+        //RODA DENTRO DOS PRODUTOS
         foreach ($request->carrinho as $produtos) {
 
             $estoque = Estoque::where('id_produto_variacoes', $produtos['id_produto_variacao'])->first();
+            $itens_em_pre_pedido = PrePedidoItens::where('id_produto_variacoes', $produtos['id_produto_variacao'])
+                ->get();
 
-            //VERIFICA SE EXISTE A QUANTIDADE SOLICITADA DE ITENS DAQUELA VARIAÇÃO EM ESTOQUE
-            if (!$estoque || ($estoque->quantidade < $produtos['quantidade'])) {
+            if (!$estoque) {
                 return response([
                     'message' => 'Produto em falta no estoque'
                 ], 400);
             }
 
-            //CASO EXISTA AQUELA QUANTIDADE, O ESTOQUE É ATUALIZADO E ADICIONADO AO PRE_PEDIDO_ITENS
-            $estoque->quantidade -= $produtos['quantidade'];
-            // $estoque->save();
+            //VERIFICA SE EXISTE A QUANTIDADE SOLICITADA DE ITENS DAQUELA VARIAÇÃO EM ESTOQUE
+            // ############ ITENS DESSE PEDIDO + ITENS DOS PRE_PEDIDOS - ITENS DO ESTOQUE ############
+            $produtos_em_estoque = $estoque->quantidade - $itens_em_pre_pedido->sum('quantidade');
 
+            if ($produtos_em_estoque - $produtos['quantidade'] < 0) {
+                return response([
+                    'message' => 'Produto em falta no estoque'
+                ], 400);
+            }
+
+            //SE SIM, CRIA O PRE_PEDIDO
             $pre_pedido_itens = new PrePedidoItens();
             $pre_pedido_itens->quantidade = $produtos['quantidade'];
             $pre_pedido_itens->id_produto_variacoes = $produtos['id_produto_variacao'];
             $pre_pedido_itens->id_pre_pedido = $pre_pedido->id;
-            // $pre_pedido->itens->save();
+            // $pre_pedido_itens->save();
+
+            // #### ANTIGA LÓGICA ####
+            //CASO EXISTA AQUELA QUANTIDADE, O ESTOQUE É ATUALIZADO E ADICIONADO AO PRE_PEDIDO_ITENS
+            // $estoque->quantidade -= $produtos['quantidade'];
+            // $estoque->save();
+            // #### ANTIGA LÓGICA ####
 
         }
 
-        //SE O USUÁRIO TIVER LOGADO, JOGAR O PRE_PEDIDO_ITENS PARA O CARRINHO TAMBÉM
-        //REDIRECIONA PARA A PÁGINA DE CHECKOUT
-        // $this->create();
+        //APÓS CRIAR O PRE_PEDIDO, RETORNA O PEDIDO,
 
         //####### PARA DEPOIS ###########
         //DEVE SER CRIADO UM JOB PARA REMOVER OS PEDIDOS EM PRE_PEDIDO QUE TENHAM MAIS DE 3 DIAS DE CRIAÇÃO
