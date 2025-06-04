ff2aca9f3c5ddba54a1160281a80c95ab903a1be
Styl Bandeira <stylbandeira@gmail.com>
2025-06-04 09:14:36 -0300

    Returns url attribute

--- app/Models/PrePedido.php
+++ app/Models/PrePedido.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Observers\PedidoObserver;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Log;
 
 class PrePedido extends Model
 {
@@ -12,10 +15,16 @@ class PrePedido extends Model
     use SoftDeletes;
 
     protected $table = 'pre_pedido';
-    protected $fillable = ['uuid'];
+    protected $fillable = ['uuid', 'codigo'];
+    protected $appends = ['pedido_url'];
 
     public function produtos()
     {
         return $this->hasMany(PrePedidoItens::class, 'id_pre_pedido', 'id');
+    }
+
+    public function getPedidoUrlAttribute()
+    {
+        return url('/pedidos/' . $this->codigo);
     }
 }
