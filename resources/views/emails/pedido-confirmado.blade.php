015052d5ac42fd84b3625a2d31529947baa2e475
Styl Bandeira <stylbandeira@gmail.com>
2025-06-04 09:17:27 -0300

    Creates pedido-confirmado view for email

--- resources/views/emails/pedido-confirmado.blade.php
+++ resources/views/emails/pedido-confirmado.blade.php
@@ -1,3 +1,6 @@
-<h1>Olá {{ $pedido['nome'] }}</h1>
+<h1>Olá {{ $pre_pedido['nome'] }}</h1>
 <p>Seu pedido foi confirmado com sucesso!</p>
-<p>Total: R$ {{ number_format($pedido['total'], 2, ',', '.') }}</p>
+<p>Total: R$ {{ number_format($pre_pedido['total'], 2, ',', '.') }}</p>
+
+
+<a href="{{ $pre_pedido['link'] }}">Link do Pedido</a>
